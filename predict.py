7e05289438500ebc57d6683703865f9ec891f116
Anouar Bouchama <76883654+anouarbouchama@users.noreply.github.com>
2023-06-14 13:38:28 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,16 @@
 from audiocraft.models import musicgen
 from audiocraft.utils.notebook import display_audio
 import torch
+from cog import BaseModel, BasePredictor, Input, Path
+
+class Output(BaseModel):
+    """
+    Output class for riffusion predictions
+    """
+
+    audio: Path
+    spectrogram: Path
+    error: T.Optional[str] = None
 
 def predict(duration,prompt):
   model = musicgen.MusicGen.get_pretrained('medium')
